4c8cd5e8ae45e40ff56a763a921f2a9634ad6c48
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-22 14:30:06 +0100

    C# for dummies book for real

---
+++ Program2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// Prompt user to enter a name.
+Console.WriteLine("Enter you name, please: ");
+
+// Now read the name enterd.
+string name = Console.ReadLine();
+
+// Greet the use with the name that was entered.
+Console.WriteLine("Hello, " + name);
+
+// Wait for user to acknowledge the results.
+Console.WriteLine("Press Enter to terminate...");
+Console.Read();
---
+++ Program3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Wait for user to acknowledge the results.
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
