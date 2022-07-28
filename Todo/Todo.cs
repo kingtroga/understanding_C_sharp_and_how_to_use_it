705ebe29d498afaa269ebcd55f5871a60c009404
Tari Yekorogha <trogaclassicman@gmail.com>
2022-07-28 02:04:05 +0100

    done with PrintMainMenu() function

--- Todo/Todo.cs
+++ Todo/Todo.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Todo
+{
+    public class Todo
+    {
+        static void Main(string[] args)
+        {
+            // Instatiate a TodoFunctions Object to 
+            // access all the functions created
+            TodoFunctions td = new TodoFunctions();
+
+            // Data Structure to store the menu
+            Dictionary<string, string> theMenu = new Dictionary<string, string>()
+            {
+                {"L", "List"},
+                {"A", "Add"},
+                {"U", "Update"},
+                {"D", "Delete"},
+                {"S", "Save the data to file"},
+                {"R", "Restore data from file"},
+                {"Q", "Quit this program"}
+            };
+
+            // intial set up of all tasks
+            List<Dictionary<string, string>> allTasks = new List<Dictionary<string,string>>();
+
+            //Data structure to store the List menu
+            // suboptions
+            Dictionary<string, string> listMenu = new Dictionary<string, string>()
+            {
+                {"A", "all tasks"},
+                {"C", "completed tasks"},
+                {"I", "incomplete tasks"}
+            };
+
+            // Data structure to store the mapping of the 
+            // the integer priority values to their textural interpretation
+            Dictionary<int, string> priorityScale = new Dictionary<int, string>()
+            {
+                {1, "Lowest"},
+                {2, "Low"},
+                {3, "Medium"},
+                {4, "High"},
+                {5, "Highest"}
+            };
+
+            string opt;
+
+            while (true)
+            {
+                // Output the main menu
+                // to the user's screen
+                td.PrintMainMenu(theMenu);
+
+                break;
+            }
+
+        }
+    }
+}
--- Todo/TodoFunctions.cs
+++ Todo/TodoFunctions.cs
@@ -8,5 +8,17 @@ namespace Todo
 {
     public class TodoFunctions
     {
+        public void PrintMainMenu(Dictionary<string, string> menu)
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("What would you like to do?");
+
+            foreach (KeyValuePair<string,string> item in menu)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+
+            Console.WriteLine("==========================");
+        }
     }
 }
