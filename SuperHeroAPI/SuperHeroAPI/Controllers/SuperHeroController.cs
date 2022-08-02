493e6dc2b8da316031cfb4c044b19a32a7d08e3d
Tari Yekorogha <trogaclassicman@gmail.com>
2022-08-02 11:35:19 +0100

    time to use code first migration

--- SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -36,7 +36,7 @@ namespace SuperHeroAPI.Controllers
         public async Task<ActionResult<SuperHero>> Get(int id)
         {
             int index = id - 1;
-            var hero = heroes[index];
+            var hero = heroes.Find(h => h.Id == id);
             if (hero == null) return BadRequest("Hero not Found.");
             return Ok(hero);
         }
@@ -48,5 +48,32 @@ namespace SuperHeroAPI.Controllers
             heroes.Add(hero);
             return Ok(heroes);
         }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null) return BadRequest("Hero not found.");
+
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+
+            return Ok(heroes);
+        }
+
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SuperHero>> Delete(int id)
+        {
+            int index = id - 1;
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null) return BadRequest("Hero not Found.");
+
+            heroes.Remove(hero);
+            return Ok(heroes);
+        }
+
     }
 }
