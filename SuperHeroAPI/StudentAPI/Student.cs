9b1abee4c4de656112d408a69f514ae2d7963bc0
Tari Yekorogha <trogaclassicman@gmail.com>
2022-08-03 14:51:03 +0100

    Done with the API

---
+++ SuperHeroAPI/StudentAPI/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentAPI
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int MatricNo { get; set; } 
+        public int Level { get; set; }
+        public string Department { get; set; } = string.Empty;
+        public string Program { get; set; } = string.Empty;
+    }
+}
