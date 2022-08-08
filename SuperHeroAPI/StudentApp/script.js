0c8895770348a7695b9d2045ede96d7a1f3a1fee
Tari Yekorogha <trogaclassicman@gmail.com>
2022-08-08 12:36:45 +0100

    Dynamically display only active students

--- SuperHeroAPI/StudentApp/script.js
+++ SuperHeroAPI/StudentApp/script.js
@@ -18,72 +18,75 @@ function displayStudents(response) {
 
     response.map((student) => {
         // create a new row on the table for each student
-        console.log(student.isActive);
-        let tableRow = document.createElement('tr');
-        let tableData1 = document.createElement('td');
-        let tableData2 = document.createElement('td');
-        let tableData3 = document.createElement('td');
-        let tableData4 = document.createElement('td');
-        let tableData5 = document.createElement('td');
-        let tableData6 = document.createElement('td');
-        let tableData7 = document.createElement('td');
-        let tableData8 = document.createElement('td');
-        let tableData9 = document.createElement('td');
+        if (student.isActive) {
+            let tableRow = document.createElement('tr');
+            let tableData1 = document.createElement('td');
+            let tableData2 = document.createElement('td');
+            let tableData3 = document.createElement('td');
+            let tableData4 = document.createElement('td');
+            let tableData5 = document.createElement('td');
+            let tableData6 = document.createElement('td');
+            let tableData7 = document.createElement('td');
+            let tableData8 = document.createElement('td');
+            let tableData9 = document.createElement('td');
 
-        /* creating the table data for each student */
-        var studentSN = document.createTextNode(`${++SN}`);
-        var studentFirstName = document.createTextNode(`${student.firstName}`);
-        var studentLastName = document.createTextNode(`${student.lastName}`);
-        var studentMatricNo = document.createTextNode(`${student.matricNo}`);
-        var studentLevel = document.createTextNode(`${student.level}`);
-        var studentDepartment = document.createTextNode(`${student.department}`);
-        var studentProgram = document.createTextNode(`${student.program}`);
+            /* creating the table data for each student */
+            var studentSN = document.createTextNode(`${++SN}`);
+            var studentFirstName = document.createTextNode(`${student.firstName}`);
+            var studentLastName = document.createTextNode(`${student.lastName}`);
+            var studentMatricNo = document.createTextNode(`${student.matricNo}`);
+            var studentLevel = document.createTextNode(`${student.level}`);
+            var studentDepartment = document.createTextNode(`${student.department}`);
+            var studentProgram = document.createTextNode(`${student.program}`);
 
-        /* edit button */
-        var editButtonText = document.createTextNode("Edit");
-        let editButton = document.createElement('button');
-        editButton.className = "edit";
-        editButton.setAttribute("onclick", "handleClickPUT(event)");
-        editButton.setAttribute("id", `${student.id}`)
-        editButton.appendChild(editButtonText);
+            /* edit button */
+            var editButtonText = document.createTextNode("Edit");
+            let editButton = document.createElement('button');
+            editButton.className = "edit";
+            editButton.setAttribute("onclick", "handleClickPUT(event)");
+            editButton.setAttribute("id", `${student.id}`)
+            editButton.appendChild(editButtonText);
 
-        /* delete button */
-        var deleteButtonText = document.createTextNode("Delete");
-        let deleteButton = document.createElement('button');
-        deleteButton.className = "delete";
-        deleteButton.setAttribute("onclick", "handleClickDelete(event)");
-        deleteButton.setAttribute("id", `${student.id}`)
-        deleteButton.appendChild(deleteButtonText);
+            /* delete button */
+            var deleteButtonText = document.createTextNode("Delete");
+            let deleteButton = document.createElement('button');
+            deleteButton.className = "delete";
+            deleteButton.setAttribute("onclick", "handleClickDelete(event)");
+            deleteButton.setAttribute("id", `${student.id}`)
+            deleteButton.appendChild(deleteButtonText);
 
-        tableData1.appendChild(studentSN);
-        tableRow.appendChild(tableData1);
+            tableData1.appendChild(studentSN);
+            tableRow.appendChild(tableData1);
 
-        tableData2.appendChild(studentFirstName);
-        tableRow.appendChild(tableData2);
+            tableData2.appendChild(studentFirstName);
+            tableRow.appendChild(tableData2);
 
-        tableData3.appendChild(studentLastName);
-        tableRow.appendChild(tableData3);
+            tableData3.appendChild(studentLastName);
+            tableRow.appendChild(tableData3);
 
-        tableData4.appendChild(studentMatricNo);
-        tableRow.appendChild(tableData4);
+            tableData4.appendChild(studentMatricNo);
+            tableRow.appendChild(tableData4);
 
-        tableData5.appendChild(studentLevel);
-        tableRow.appendChild(tableData5);
+            tableData5.appendChild(studentLevel);
+            tableRow.appendChild(tableData5);
 
-        tableData6.appendChild(studentDepartment);
-        tableRow.appendChild(tableData6);
+            tableData6.appendChild(studentDepartment);
+            tableRow.appendChild(tableData6);
 
-        tableData7.appendChild(studentProgram);
-        tableRow.appendChild(tableData7);
+            tableData7.appendChild(studentProgram);
+            tableRow.appendChild(tableData7);
 
-        tableData8.appendChild(editButton);
-        tableRow.appendChild(tableData8);
+            tableData8.appendChild(editButton);
+            tableRow.appendChild(tableData8);
 
-        tableData9.appendChild(deleteButton);
-        tableRow.appendChild(tableData9);
+            tableData9.appendChild(deleteButton);
+            tableRow.appendChild(tableData9);
 
-        studentTable.appendChild(tableRow);   
-    })
+            studentTable.appendChild(tableRow);   
+    }
+})
+
+        
 }
 
 
